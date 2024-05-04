6c47bb8308a5c7ca66e99c3104e03fda2f30ca07
literallyvoid <literallyvoid@gmail.com>
2024-05-04 11:41:33 -0700

    Move loadArrayBuffer into its own file

--- src/main.js
+++ src/main.js
@@ -1,38 +1,7 @@
 import { Table } from "./table.js";
 import { input, visualization, templates } from "./app-document.js";
 import { codepointUtf8Units, codepointUtf16Units, segmentGraphemes } from "./segment.js";
-
-/**
-  @param {string} url
-  @param {(progress: number) => void} progress_callback
-  */
-async function loadArrayBuffer(url, progress_callback) {
-	const req = new XMLHttpRequest();
-	req.responseType = "arraybuffer";
-	req.addEventListener("progress", (ev) => {
-		progress_callback(ev.loaded / ev.total);
-	});
-
-	const promise = new Promise((resolve, reject) => {
-		req.addEventListener("load", (_) => {
-			progress_callback(1);
-			resolve(req.response);
-		})
-
-		req.addEventListener("error", (_) => {
-			reject(new Error(`failed to load ${url}: ${req.status} ${req.statusText}`));
-		})
-
-		req.addEventListener("abort", (_) => {
-			reject(new Error(`failed to load ${url}: abort`));
-		})
-	});
-
-	req.open("GET", url);
-	req.send();
-
-	return promise;
-}
+import { loadArrayBuffer } from "./network.js";
 
 /** @type {Table | null} */
 let table = null;
